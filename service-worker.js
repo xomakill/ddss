722d5328b01e6f71030e69a2249835f08542e854
xomakill <82303962+xomakill@users.noreply.github.com>
2026-03-15 21:47:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,28 @@
-const CACHE_NAME = 'ukushuka-cache-v1'; // Имя вашего кэша
-const urlsToCache = [
-  '/',
-  '/index.html',
-  // Добавьте сюда другие важные статические файлы, которые должны быть доступны офлайн
-  // Например: '/static/js/bundle.js', '/static/css/main.css', '/ukushuka-logo.png'
-  // Эти пути могут отличаться в зависимости от вашей сборки React-приложения
-  '/favicon.ico',
-  '/logo192.png',
-  '/logo512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Открыт кэш');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Если запрос есть в кэше, отдаем его
-        if (response) {
-          return response;
-        }
-        // Иначе делаем сетевой запрос
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          // Удаляем старые кэши
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+{
+  "name": "CAR DOCTOR PMR",
+  "short_name": "CAR-DOC",
+  "description": "Мобильное приложение для вызова помощи вашему авто!",
+  "scope": "/",
+  "display": "standalone",
+  "orientation": "portrait",
+  "theme_color": "#dc2626",
+  "background_color": "#ffffff",
+  "icons": [
+    {
+      "src": "https://car-doctor.unaux.com/images/web-app-manifest-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://car-doctor.unaux.com/images/web-app-manifest-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "https://car-doctor.unaux.com/images/web-app-manifest-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ]
+}
